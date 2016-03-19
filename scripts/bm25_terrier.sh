6caacb3f11e2c21c43d7194518bca1483a8e66ed
Santiago Gonzalez Toral <hernangt12re3@gmail.com>
2016-03-19 17:01:13 +0000

    Scripts for indexing and retrieval using BM25 in Terrier

--- scripts/bm25_terrier.sh
+++ scripts/bm25_terrier.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 if [ ! -n "$TERRIER_HOME" ];
 then
@@ -11,17 +11,40 @@ then
 	TERRIER_HOME=$home 
 fi
 
-read -p "Enter absoulte path for Topics file: " TOPICS_FILE
+read -p "Enter absoulte path for adhoc topics file (default: TERRIER_HOME/trec2013-topics.txt): " TOPICS_FILE
 
 if [ ! -n "$TOPICS_FILE" ];
 then
-	TOPICS_FILE="/Users/santteegt/Clueweb/topics/trec2013-topics.txt"
+	TOPICS_FILE=$TERRIER_HOME"/trec2013-topics.txt"
 fi
+
+read -p "Enter absoulte path for adhoc judgement (qrels) file (default: TERRIER_HOME/qrels.adhoc.txt): " QRELS_FILE
+
+if [ ! -n "$QRELS_FILE" ];
+then
+	QRELS_FILE=$TERRIER_HOME"/qrels.adhoc.txt"
+fi
+
+
 
 TERRIER_PROP=$TERRIER_HOME"/etc/terrier.properties"
 
+echo "\n#RETRIEVAL properties" >> $TERRIER_PROP
+echo "#Adhoc topic parser conf" >> $TERRIER_PROP
 echo "trec.topics="$TOPICS_FILE >> $TERRIER_PROP
+echo "trec.topics.parser=TRECQuery" >> $TERRIER_PROP
+echo "TrecQueryTags.doctag=TOP" >> $TERRIER_PROP
+echo "TrecQueryTags.idtag=NUM" >> $TERRIER_PROP
+echo "TrecQueryTags.process=TOP,NUM,TITLE" >> $TERRIER_PROP
+echo "#Retrieval model conf" >> $TERRIER_PROP
 echo "trec.model=org.terrier.matching.models.BM25" >> $TERRIER_PROP
+echo "trec.querying.outputformat.docno.meta.key=filename" >> $TERRIER_PROP
+echo "#trec.results=PATH_TO_RESULTS_DIR" >> $TERRIER_PROP
+echo "#Evaluation conf" >> $TERRIER_PROP
+echo "#Relevance judgement files for adhoc evaluation" >> $TERRIER_PROP
+echo "trec.qrels="$QRELS_FILE >> $TERRIER_PROP
+
+echo "#end of coursework conf." >> $TERRIER_PROP
 
 #RUNNING THE RETRIEVAL PROCESS BASED ON TOPIC FILES
 $TERRIER_HOME/bin/trec_terrier.sh -r
--- scripts/index_clueweb.sh
+++ scripts/index_clueweb.sh
@@ -11,85 +11,44 @@ then
 	TERRIER_HOME=$home 
 fi
 
-read -p "Enter absoulte path for Clueweb data files (default: current_dir/clueweb12): " CLUEWEB_DIR
-
-if [ ! -n "$CLUEWEB_HOME" ];
-then
-	CLUEWEB_DIR=`pwd`"/clueweb12"
-fi
-
 #CONFIGURING TERRIER FOR TREC
-$TERRIER_HOME"/bin/trec_setup.sh" $CLUEWEB_DIR
-find $CLUEWEB_DIR -type f | sort | grep -v info > $TERRIER_HOME"/etc/collection.spec"
+#$TERRIER_HOME"/bin/trec_setup.sh" $CLUEWEB_DIR
+#find $CLUEWEB_DIR -type f | sort | grep -v info > $TERRIER_HOME"/etc/collection.spec"
 
 #SETTING TERRIER PROPERTIES DIR
 TERRIER_PROP=$TERRIER_HOME"/etc/terrier.properties"
-rm $TERRIER_PROP
-
-
-####################################################################################
-############################ TREC DEFAULT CONFIGURATION ############################
-####################################################################################
-echo "#default controls for query expansion" >> $TERRIER_PROP
-echo "querying.postprocesses.order=QueryExpansion" >> $TERRIER_PROP
-echo "querying.postprocesses.controls=qe:QueryExpansion" >> $TERRIER_PROP
-echo "#default controls for the web-based interface. SimpleDecorate" >> $TERRIER_PROP
-echo "#is the simplest metadata decorator. For more control, see Decorate." >> $TERRIER_PROP
-echo "querying.postfilters.order=SimpleDecorate,SiteFilter,Scope" >> $TERRIER_PROP
-echo "querying.postfilters.controls=decorate:SimpleDecorate,site:SiteFilter,scope:Scope" >> $TERRIER_PROP
-
-echo "#default and allowed controls" >> $TERRIER_PROP
-echo "querying.default.controls=" >> $TERRIER_PROP
-echo "querying.allowed.controls=scope,qe,qemodel,start,end,site,scope" >> $TERRIER_PROP
-
-echo "#document tags specification" >> $TERRIER_PROP
-echo "#for processing the contents of" >> $TERRIER_PROP
-echo "#the documents, ignoring DOCHDR" >> $TERRIER_PROP
-echo "TrecDocTags.doctag=DOC" >> $TERRIER_PROP
-echo "TrecDocTags.idtag=DOCNO" >> $TERRIER_PROP
-echo "#TrecDocTags.skip=DOCHDR #commented" >> $TERRIER_PROP
-echo "#set to true if the tags can be of various case" >> $TERRIER_PROP
-echo "TrecDocTags.casesensitive=false" >> $TERRIER_PROP
-
-echo "#set of tags to process" >> $TERRIER_PROP
-echo "TrecQueryTags.process=TOP,NUM,TITLE" >> $TERRIER_PROP
-echo "#set of tags to skip" >> $TERRIER_PROP
-echo "TrecQueryTags.skip=DESC,NARR" >> $TERRIER_PROP
-
-echo "#stop-words file" >> $TERRIER_PROP
-echo "stopwords.filename=stopword-list.txt" >> $TERRIER_PROP
-
-echo "#the processing stages a term goes through" >> $TERRIER_PROP
-echo "termpipelines=Stopwords,PorterStemmer" >> $TERRIER_PROP
+#rm $TERRIER_PROP
 
 
 ####################################################################################
 ############################CONFIGURATION FOR COURSEWORK############################
 ####################################################################################
-echo "#Coursework 1 configuration" >> $TERRIER_PROP
-echo "#terrier home directory" >> $TERRIER_PROP
-#echo 'terrier.home="/Users/santteegt/GitRepositories/irdm-terrier-trec"' >> $TERRIER_PROP
+#initial coursework conf
+
+echo "\n#INDEXING properties" >> $TERRIER_PROP
+
+echo "#Fields to be added to the index" >> $TERRIER_PROP
+echo "FieldTags.process=TITLE,H1, H2, H3, H4, H5, H6, ELSE" >> $TERRIER_PROP
+echo "#Collection docs type and Parser" >> $TERRIER_PROP
 echo "trec.collection.class=SimpleFileCollection" >> $TERRIER_PROP
-echo "indexing.simplefilecollection.extensionsparsers=txt:TaggedDocument" >> $TERRIER_PROP
-echo "#Tokeniser class declared explicitly" >> $TERRIER_PROP
-echo "tokeniser=org.terrier.indexing.tokenisation.EnglishTokeniser" >> $TERRIER_PROP
+echo "indexing.simplefilecollection.extensionparsers=txt:TaggedDocument" >> $TERRIER_PROP
+echo "#Stop words and term pipelines for parsing stage" >> $TERRIER_PROP
+echo "stopwords.filename=stopword-list.txt" >> $TERRIER_PROP
+echo "termpipelines=Stopwords ,PorterStemmer" >> $TERRIER_PROP
+echo "#Storing the filename into the index" >> $TERRIER_PROP
+echo "indexer.meta.forward.keys=filename" >> $TERRIER_PROP
+echo "indexer.meta.reverse.keys=filename" >> $TERRIER_PROP
+echo "indexer.meta.forward.keylens=64" >> $TERRIER_PROP
+
 echo "#convert document words to lowercase" >> $TERRIER_PROP
 echo "lowercase=true" >> $TERRIER_PROP
+echo "#Tokeniser class declared explicitly" >> $TERRIER_PROP
+echo "tokeniser=org.terrier.indexing.tokenisation.EnglishTokeniser" >> $TERRIER_PROP
 echo "#prefix for index filenames" >> $TERRIER_PROP
 echo "terrier.index.prefix=terrier_clueweb_index" >> $TERRIER_PROP
-echo "#indexer properties" >> $TERRIER_PROP
-echo "indexer.meta.forward.keys=docno" >> $TERRIER_PROP
-echo "indexer.meta.forward.keylens=26" >> $TERRIER_PROP
-echo "#unique id for documents" >> $TERRIER_PROP
-echo "indexer.meta.reverse.keys=docno" >> $TERRIER_PROP
-echo "#tags to skip" >> $TERRIER_PROP
-echo "TrecDocTags.skip=script,style" >> $TERRIER_PROP
 
-echo "#denotes the beginning of the document" >> $TERRIER_PROP
-echo "TrecQueryTags.doctag=title" >> $TERRIER_PROP
-echo "#tag to use as unique identifier" >> $TERRIER_PROP
-echo "TrecQueryTags.idtag=num" >> $TERRIER_PROP
+echo "#end of coursework conf." >> $TERRIER_PROP
 
 #RUNNING THE INDEXING
-$TERRIER_HOME/bin/trec_terrier.sh -i -j
+$TERRIER_HOME/bin/trec_terrier.sh -i
 
